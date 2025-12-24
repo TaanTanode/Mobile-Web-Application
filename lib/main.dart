08bc1cd676143b2cab43ff5b4a077106dfb23de3
TaanTanode <taantanode1428@gmail.com>
2025-12-24 11:43:22 +0700

    new complete 24/12

--- lib/main.dart
+++ lib/main.dart
@@ -10,30 +10,105 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-          useMaterial3: true,
-          appBarTheme: const AppBarTheme(color: Colors.lightGreenAccent)),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('MyApp'),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 23, 7, 255)),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          color: Color.fromARGB(255, 79, 111, 255),
         ),
-        body: Center(
+      ),
+      home: Scaffold(
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Text(
-                'Hello Cs MB test',
+              Card(
+                color: Colors.grey[200],
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.network(
+                            'https://img.pikbest.com/png-images/qiantu/hand-drawn-cartoon-cute-child-little-girl-half-body-decorative-pattern_2616393.png!sw800',
+                            height: 50,
+                            width: 50,
+                            fit: BoxFit.cover,
+                          ),
+                          const SizedBox(width: 10),
+                          const Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'โครงการคนละครึ่งพลัส สนับสนุนโดยภาครัฐ มาตรการกระตุ้นเศรษฐกิจ 50-50%',
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'ยอดคงเหลือสิทธิ์วันนี้',
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  '50 บาท',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              ElevatedButton(
-                  onPressed: () {
-                    debugPrint("btn pressed");
-                  },
-                  child: Text(
-                    "OK button",
-                  ))
-              // Text(
-              //   'MB test',
-              // ),
+              const SizedBox(height: 10),
+              const Card(
+                color: Color.fromARGB(255, 233, 233, 233),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(
+                        children: [
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  'ยอดใช้สิทธิ์แล้ววันนี้',
+                                  style: TextStyle(fontSize: 14),
+                                ),
+                                Text(
+                                  '150 บาท',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                ),
+              ),
             ],
           ),
         ),
